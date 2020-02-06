050bce3e74887d7b09ac79ba0c0ca0144a05151d
Brandon K. Waltz <bkwaltz@colby.edu>
2020-02-06 15:20:12 -0500

    adds gulp for baseinstall

--- scripts/digitalservice/dependencies-run-install-build.sh
+++ scripts/digitalservice/dependencies-run-install-build.sh
@@ -7,6 +7,12 @@ shopt -s extglob # Turns on extended globbing
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
+# base-install
+cd web/wp-content/themes/baseinstall
+npm install
+gulp
+cd -
+
 NPM_DIRS=`ls web/wp-content/@(plugins|themes)/colby-*/src/@(index.js)` # Saves it to a variable
 for NPMDIR in $NPM_DIRS; do
   NPMDIR=`dirname $NPMDIR`
@@ -18,9 +24,6 @@ for NPMDIR in $NPM_DIRS; do
   npm run build
   cd -
 done
-
-# install npm dependencies in the app root
-npm install
 
 # npm install
 shopt -u extglob
