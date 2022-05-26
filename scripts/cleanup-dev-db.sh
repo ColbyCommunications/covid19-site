e7f4281475f774f4746349e7a6ad2f75e2af2bb7
Brandon K. Waltz <bwaltz6@gmail.com>
2022-05-26 13:45:40 -0400

    cleanup-dev-db.sh script

--- scripts/cleanup-dev-db.sh
+++ scripts/cleanup-dev-db.sh
@@ -3,7 +3,6 @@
 DEV_HOSTNAME=$(echo $PLATFORM_ROUTES | base64 --decode | jq 'keys[0]' | tr -d \")
 
 if [ "${PLATFORM_BRANCH}" != master ]; then
-  echo "Running: wp search-replace 'https://${PRIMARY_DOMAIN}/' '${DEV_HOSTNAME}' wp_posts wp_postmeta wp_options"
-  wp search-replace "https://${PRIMARY_DOMAIN}/" "${DEV_HOSTNAME}" wp_posts wp_postmeta wp_options wp_yoast_indexable
-  mysql -h database.internal -P 3306 -u user -e "TRUNCATE TABLE main.wp_yoast_indexable"
+  echo "Running: wp search-replace 'https://${PRIMARY_DOMAIN}/' '${DEV_HOSTNAME}' --all-tables"
+  wp search-replace "https://${PRIMARY_DOMAIN}/" "${DEV_HOSTNAME}" --all-tables
 fi
